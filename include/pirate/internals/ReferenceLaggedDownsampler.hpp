kmsmith137/pirate

commit 7c8e75d9e5da23cd92f4f054b6fb79bcd061d8e5

improve ReferenceLaggedDownsampler::apply() so that input/output arrays can be non-contiguous

src_lib/ReferenceLaggedDownsampler.cu
@@ -96,11 +96,11 @@ void ReferenceLaggedDownsampler::apply(const Array<float> &in, Array<float> *out
 
     _check_shape("in", in, nbeams, pow2(r), ntime);
     _check_shape("out", outp[0], nbeams, pow2(r-1), xdiv(ntime,2));
-    
-    // Input/output arrays, reshaped to 2-d.
-    Array<float> in_2d = in.reshape_ref({ nbeams * pow2(r), ntime });
-    Array<float> out_2d = outp[0].reshape_ref({ nbeams * pow2(r-1), ntime/2 });
 
+    // Reshape input array to 2-d, since reference_downsample_time() assumes a 2-d array.
+    Array<float> in_2d = in.clone();   // copy, to avoid reshape failure if strides are non-contiguous
+    in_2d = in_2d.reshape_ref({ nbeams * pow2(r), ntime });
+    
     // Reshaped time-downsampled input array: (nbeams * 2^r, ntime/2)
     Array<float> in_ds({ nbeams * pow2(r), ntime/2 }, af_uhost | af_zero);
     reference_downsample_time(in_2d, in_ds, false);  // normalize=false, i.e. sum with no factor 0.5
@@ -110,9 +110,14 @@ void ReferenceLaggedDownsampler::apply(const Array<float> &in, Array<float> *out
     lagbuf_small->apply_lags(in_ds2);
 
     // Downsample in frequency, and apply "large" lags.
-    reference_downsample_freq(in_ds2, out_2d, false);   // normalize=false, i.e. sum with no factor 0.5
-    lagbuf_large->apply_lags(out_2d);
-
+    Array<float> out_tmp({ nbeams * pow2(r-1), ntime/2 }, af_uhost | af_zero);
+    reference_downsample_freq(in_ds2, out_tmp, false);   // normalize=false, i.e. sum with no factor 0.5
+    lagbuf_large->apply_lags(out_tmp);
+    
+    // Reshape output_tmp array from 2-d to 3-d, and copy to caller-specified array.
+    out_tmp = out_tmp.reshape_ref({ nbeams, pow2(r-1), ntime/2 });
+    outp[0].fill(out_tmp);
+    
     if (params.num_downsampling_levels == 1)
 	return;
     

commit ea6848a10171c8e3bb895a5f4c9e8843db71052e

fix bug in ReferenceLaggedDownsampler::apply() introduced in commit 7c8e75d...

include/pirate/internals/ReferenceLaggedDownsampler.hpp
@@ -33,18 +33,26 @@ struct ReferenceLaggedDownsampler
 	long ntime = 0;
     };
 
+    ReferenceLaggedDownsampler(const Params &params);
+
+    // apply(): the input/output arrays have the following shapes:
+    //
+    //   in.shape = (nbeams, 2^large_input_rank, ntime)
+    //   out[i].shape = (nbeams, 2^(large_input_rank-1), ntime/2^(i+1))
+    //   out.size() = Params::num_downsampling_levels
+    //
+    // Note: if nbeams == 1, then the beam axis can be omitted, i.e. the following are okay:
+    //   in.shape = (2^large_input_rank, ntime)
+    //   out[i].shape = (2^(large_input_rank-1), ntime/2^(i+1))
+    
+    void apply(const gputils::Array<float> &in, std::vector<gputils::Array<float>> &out);
+    void apply(const gputils::Array<float> &in, gputils::Array<float> *outp);
+
     const Params params;
 
     std::shared_ptr<ReferenceLagbuf> lagbuf_small;
     std::shared_ptr<ReferenceLagbuf> lagbuf_large;
     std::shared_ptr<ReferenceLaggedDownsampler> next;
-
-    ReferenceLaggedDownsampler(const Params &params);
-
-    // 'out' has outer length nds = Params::num_downsampling_levels.
-    // out[i] has shape (nbeams, 2^(large_input_rank-1), ntime/2^(i+1)).
-    void apply(const gputils::Array<float> &in, std::vector<gputils::Array<float>> &out);
-    void apply(const gputils::Array<float> &in, gputils::Array<float> *outp);
 };
 
 

src_lib/ReferenceLaggedDownsampler.cu
@@ -90,10 +90,21 @@ static void _check_shape(const char *name, const Array<float> &arr, ssize_t nbea
 
 void ReferenceLaggedDownsampler::apply(const Array<float> &in, Array<float> *outp)
 {
+    // Reminder: the input/output arrays have the following shapes:
+    //
+    //   in.shape = (nbeams, 2^large_input_rank, ntime)
+    //   out[i].shape = (nbeams, 2^(large_input_rank-1), ntime/2^(i+1))
+    //   out.size() = Params::num_downsampling_levels
+    //
+    // Note: if nbeams == 1, then the beam axis can be omitted, i.e. the following are okay:
+    //   in.shape = (2^large_input_rank, ntime)
+    //   out[i].shape = (2^(large_input_rank-1), ntime/2^(i+1))
+
     int r = params.large_input_rank;
     int nbeams = params.nbeams;
     long ntime = params.ntime;
 
+    // Note: _check_shape() allows beam axis to be omitted if nbeams == 1.
     _check_shape("in", in, nbeams, pow2(r), ntime);
     _check_shape("out", outp[0], nbeams, pow2(r-1), xdiv(ntime,2));
 
@@ -114,8 +125,10 @@ void ReferenceLaggedDownsampler::apply(const Array<float> &in, Array<float> *out
     reference_downsample_freq(in_ds2, out_tmp, false);   // normalize=false, i.e. sum with no factor 0.5
     lagbuf_large->apply_lags(out_tmp);
     
-    // Reshape output_tmp array from 2-d to 3-d, and copy to caller-specified array.
-    out_tmp = out_tmp.reshape_ref({ nbeams, pow2(r-1), ntime/2 });
+    // Reshape output_tmp array from 2-d to target shape, and copy to caller-specified array.
+    // Note that the call to reshape_ref() correctly handles the case where nbeams==1, and the caller
+    // has chosen to omit the beam axis (see "reminder" above).
+    out_tmp = out_tmp.reshape_ref(outp[0].ndim, outp[0].shape);
     outp[0].fill(out_tmp);
     
     if (params.num_downsampling_levels == 1)
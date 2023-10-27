ea6848a10171c8e3bb895a5f4c9e8843db71052e
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2023-10-27 15:51:32 +0000

    fix bug in ReferenceLaggedDownsampler::apply() introduced in commit 7c8e75d...

--- include/pirate/internals/ReferenceLaggedDownsampler.hpp
+++ include/pirate/internals/ReferenceLaggedDownsampler.hpp
@@ -33,18 +33,26 @@ struct ReferenceLaggedDownsampler
 	long ntime = 0;
     };
 
+    ReferenceLaggedDownsampler(const Params &params);
+
+    // apply(): the input/output arrays have the following shapes:
+    //
+    //   in.shape = (nbeams, 2^large_input_rank, ntime)
+    //   out[i].shape = (nbeams, 2^(large_input_rank-1), ntime/2^(i+1))
+    //   out.size() = Params::num_downsampling_levels
+    //
+    // Note: if nbeams == 1, then the beam axis can be omitted, i.e. the following are okay:
+    //   in.shape = (2^large_input_rank, ntime)
+    //   out[i].shape = (2^(large_input_rank-1), ntime/2^(i+1))
+    
+    void apply(const gputils::Array<float> &in, std::vector<gputils::Array<float>> &out);
+    void apply(const gputils::Array<float> &in, gputils::Array<float> *outp);
+
     const Params params;
 
     std::shared_ptr<ReferenceLagbuf> lagbuf_small;
     std::shared_ptr<ReferenceLagbuf> lagbuf_large;
     std::shared_ptr<ReferenceLaggedDownsampler> next;
-
-    ReferenceLaggedDownsampler(const Params &params);
-
-    // 'out' has outer length nds = Params::num_downsampling_levels.
-    // out[i] has shape (nbeams, 2^(large_input_rank-1), ntime/2^(i+1)).
-    void apply(const gputils::Array<float> &in, std::vector<gputils::Array<float>> &out);
-    void apply(const gputils::Array<float> &in, gputils::Array<float> *outp);
 };
 
 
