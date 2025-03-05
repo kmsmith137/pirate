484707f4daa156e24c6423dcaccfea5b5d683dc6
Kendrick Smith <kmsmith@cx67.ctc.chord-observatory.ca>
2025-03-05 16:08:57 +0000

    reorganize source files related to dedispersion kernels

---
+++ include/pirate/internals/DedispersionKernel.hpp
@@ -0,0 +1,138 @@
+#ifndef _PIRATE_INTERNALS_DEDISPERSION_KERNEL_HPP
+#define _PIRATE_INTERNALS_DEDISPERSION_KERNEL_HPP
+
+#include <ksgpu/Array.hpp>
+#include "ReferenceTree.hpp"
+#include "ReferenceLagbuf.hpp"
+
+namespace pirate {
+#if 0
+}  // editor auto-indent
+#endif
+
+
+// The meaning of Params::apply_residual_lags needs some explanation!
+//
+// This is used in the second dedisperser stage, where each tree channel is labelled
+// by two indices:
+//
+//   - a bit-reversed DM 0 <= d < 2^(total_rank-active_rank)
+//   - a coarse frequency 0 <= f < 2^(active_rank).
+//
+// Before dedispersing the data, the following residual lag is applied:
+//
+//   int lag = rb_lag(f, d, total_rank-active_rank, active_rank, params.input_is_downsampled_tree);
+//   int residual_lag = lag % nelts_per_segment;
+
+struct DedispersionKernelParams
+{
+    ksgpu::Dtype dtype;   // either "float32" or "float16"
+    int rank = -1;        // satisfies 1 <= rank <= 8
+    int nambient = 0;
+    int total_beams = 0;
+    int beams_per_batch = 0;
+    int ntime = 0;        // includes downsampling factor, if any
+    
+    // Input/output buffer types.
+    bool input_is_ringbuf = false;
+    bool output_is_ringbuf = false;
+    
+    // Residual lags (see comment above).
+    bool apply_input_residual_lags = false;
+    bool input_is_downsampled_tree = false;   // only matters if apply_input_residual_lags=true
+    int nelts_per_segment = 0;                // only matters if apply_input_residual_lags=true
+
+    // Only used if (input_is_ringbuf || output_is_ringbuf)
+    ksgpu::Array<uint> ringbuf_locations;
+    long ringbuf_nseg = 0;
+    
+    // Throws an exception if anything is wrong.
+    void validate(bool on_gpu) const;
+};
+
+
+struct ReferenceDedispersionKernel
+{
+    using Params = DedispersionKernelParams;
+
+    ReferenceDedispersionKernel(const Params &params);
+
+    const Params params;
+
+    // The 'in' and 'out' arrays are either dedispersion buffers or ringbufs, depending on
+    // values of Params::input_is_ringbuf and Params::output_is_ringbuf. Shapes are:
+    //
+    //   - dedispersion buffer has shape (params.beams_per_batch, nambient, pow2(rank), ntime).
+    //   - ringbuf has 1-d shape (params.ringbuf_nseg * params.nelts_per_segment,)
+    //
+    // The 'itime' and 'ibeam' arguments are not logically necessary, but enable a debug check.
+    // Warning: if 'in' is not a ringbuf, then apply() may modify 'in'!
+    
+    void apply(ksgpu::Array<float> &in, ksgpu::Array<float> &out, long itime, long ibeam);
+
+    // A bit awkward -- number of trees is (total_beams / beams_per_batch).
+    std::vector<std::shared_ptr<ReferenceTree>> trees;
+    std::vector<std::shared_ptr<ReferenceLagbuf>> rlag_bufs;   // only used if params.apply_input_residual_lags == true.
+
+    // Enables a debug check.
+    long expected_itime = 0;
+    long expected_ibeam = 0;
+
+    // Used internally
+    void _copy_to_ringbuf(const ksgpu::Array<float> &in, ksgpu::Array<float> &out, long rb_pos);
+    void _copy_from_ringbuf(const ksgpu::Array<float> &in, ksgpu::Array<float> &out, long rb_pos);
+};
+
+
+// The GpuDedispersionKernel uses externally-allocated buffers for its inputs/outputs,
+// but internally allocates and manages its persistent state ("rstate").
+
+class GpuDedispersionKernel
+{
+    using Params = DedispersionKernelParams;
+    
+public:   
+    // To construct GpuDedispersionKernel instances, call this function.
+    static std::shared_ptr<GpuDedispersionKernel> make(const Params &params);
+    
+    Params params;
+    
+    // Used internally by GpuDedispersionKernel::launch().
+    int state_nelts_per_beam = 0;
+    int warps_per_threadblock = 0;
+    int shmem_nbytes = 0;
+    
+    // launch(): asynchronously launch dedispersion kernel, and return without synchronizing stream.
+    //
+    // The 'in' array has different meanings, depending on Params::input_is_ringbuf:
+    //   - If (!input_is_ringbuf): shape is (nbeams, nambient, pow2(rank), ntime).
+    //   - If (input_is_ringbuf): shape is (ntime/nelts_per_segment, nambient, pow2(rank), 4).
+    //
+    // Similarly, the 'out' array has different meanings, depending on Params::output_is_ringbuf:
+    //   - If (!output_is_ringbuf): shape is (nbeams, nambient, pow2(rank), ntime).
+    //   - If (output_is_ringbuf): shape is (ntime/nelts_per_segment, nambient, pow2(rank), 4).
+    //
+    // The 'itime' and 'ibeam' arguments are not logically necessary, but enable a debug check.
+
+    virtual void launch(const ksgpu::Array<void> &in, ksgpu::Array<void> &out, long itime, long ibeam, cudaStream_t stream=nullptr) = 0;
+
+protected:
+    // Don't call constructor directly -- call GpuDedispersionKernel::make() instead!
+    GpuDedispersionKernel(const Params &params);
+
+    // Shape (total_beams, state_nelts_per_beam).
+    ksgpu::Array<void> persistent_state;
+
+    // Enables a debug check.
+    long expected_itime = 0;
+    long expected_ibeam = 0;
+
+    // FIXME only on current cuda device (at time of construction).
+    // FIXME should either add run-time check, or switch to using constant memory.
+    ksgpu::Array<uint> integer_constants;
+};
+
+
+}  // namespace pirate
+
+#endif // _PIRATE_INTERNALS_DEDISPERSION_KERNEL_HPP
---
+++ src_lib/DedispersionKernelParams.cu
@@ -0,0 +1,67 @@
+#include "../include/pirate/internals/DedispersionKernel.hpp"
+#include "../include/pirate/internals/inlines.hpp"   // pow2()
+#include "../include/pirate/constants.hpp"
+
+#include <ksgpu/Dtype.hpp>
+#include <ksgpu/xassert.hpp>
+
+using namespace std;
+using namespace ksgpu;
+
+namespace pirate {
+#if 0
+}  // editor auto-indent
+#endif
+
+
+void DedispersionKernelParams::validate(bool on_gpu) const
+{
+    xassert_ge(rank, 0);
+    xassert_le(rank, 8);
+    xassert_gt(nambient, 0);
+    xassert_gt(total_beams, 0);
+    xassert_gt(beams_per_batch, 0);
+    xassert_le(beams_per_batch, constants::cuda_max_y_blocks);
+    xassert_ge(ntime, 0);
+
+    xassert((dtype == Dtype::native<float>()) || (dtype == Dtype::native<__half>()));
+    xassert(!input_is_ringbuf || !output_is_ringbuf);
+    
+    // Not really necessary, but failure probably indicates an unintentional bug.
+    xassert(is_power_of_two(nambient));
+    
+    // Currently assumed throughout the pirate code.
+    xassert_divisible(total_beams, beams_per_batch);
+
+    // Currently assumed by the GPU kernels.
+    int nelts_per_cache_line = xdiv(8 * constants::bytes_per_gpu_cache_line, dtype.nbits);
+    xassert_eq(nelts_per_segment, nelts_per_cache_line);
+    xassert_divisible(ntime, nelts_per_segment);
+    
+    if (input_is_ringbuf || output_is_ringbuf) {
+	long nseg = xdiv(ntime,nelts_per_segment) * nambient * pow2(rank);
+	xassert_shape_eq(ringbuf_locations, ({ nseg, 4 }));
+	xassert(ringbuf_locations.is_fully_contiguous());
+	xassert(ringbuf_nseg > 0);
+	xassert(ringbuf_nseg <= UINT_MAX);
+
+	if (on_gpu) {
+	    xassert(ringbuf_locations.on_gpu());
+	    return;
+	}
+
+	xassert(ringbuf_locations.on_host());
+	
+	for (long iseg = 0; iseg < nseg; iseg++) {
+	    const uint *rb_locs = ringbuf_locations.data + (4*iseg);
+	    long rb_offset = rb_locs[0];  // in segments, not bytes
+	    // long rb_phase = rb_locs[1];   // index of (time chunk, beam) pair, relative to current pair
+	    long rb_len = rb_locs[2];     // number of (time chunk, beam) pairs in ringbuf (same as Ringbuf::rb_len)
+	    long rb_nseg = rb_locs[3];    // number of segments per (time chunk, beam) (same as Ringbuf::nseg_per_beam)
+	    xassert(rb_offset + (rb_len-1)*rb_nseg < ringbuf_nseg);
+	}
+    }
+}
+
+
+}  // namespace pirate
