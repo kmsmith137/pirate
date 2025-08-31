1e6d16f780156424d92ed2ead24b3a4ba75da76b
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2025-08-31 18:48:36 +0000

    dd kernel refactoring: clean up test_gpu_dedispersion_kernels.cu

--- src_lib/test_gpu_dedispersion_kernels.cu
+++ src_lib/test_gpu_dedispersion_kernels.cu
@@ -17,10 +17,7 @@ namespace pirate {
 #endif
 
 
-// FIXME buffer managment (in particular the _setup_io_arrays() function) in this
-// code looks dubious -- revisit at some point.
-
-struct TestInstanceDK
+struct DedispTestInstance
 {
     // Reminder: includes 'dtype', 'input_is_ringbuf', 'output_is_ringbuf'.
     DedispersionKernelParams params;
@@ -149,7 +146,7 @@ struct TestInstanceDK
 	}
     }
 
-    
+
     void randomize_strides()
     {
 	xassert(params.nspec == 1);  // FIXME for now
@@ -179,34 +176,85 @@ struct TestInstanceDK
 };
 
 
-// Helper for run_test()
-static void _setup_io_arrays(Array<void> &in, Array<void> &out, const Array<void> &in_big, const Array<void> &out_big, const TestInstanceDK &tp, bool on_gpu)
+// Another helper class.
+struct TestArrays
 {
-    xassert(tp.params.nspec == 1);  // FIXME for now
+    DedispTestInstance tp;
+    long nbatches;
     
-    int aflags = (on_gpu ? af_gpu : af_uhost) | af_zero;
-    vector<long> istrides = on_gpu ? tp.gpu_istrides : tp.cpu_istrides;
-    vector<long> ostrides = on_gpu ? tp.gpu_ostrides : tp.cpu_ostrides;
-    vector<long> small_shape = { tp.params.beams_per_batch, pow2(tp.params.amb_rank), pow2(tp.params.dd_rank), tp.params.ntime };
+    Array<void> big_inbuf;      // either a "big" ddbuf (nchunks), or a ringbuf
+    Array<void> big_outbuf;     // either a "big" ddbuf (nchunks), or a ringbuf
+    Array<void> active_inbuf;   // either a "small" ddbuf (1 chunk), or a reference to 'big_inbuf'
+    Array<void> active_outbuf;  // either a "small" ddbuf (1 chunk), or a reference to 'big_outbuf'
     
-    if (tp.params.input_is_ringbuf)
-	in = in_big;
-    else
-	in = Array<void> (in_big.dtype, small_shape, istrides, aflags);
+    TestArrays(const DedispTestInstance &tp_, const Dtype &dtype, bool on_gpu) :
+	tp(tp_),
+	nbatches(xdiv(tp.params.total_beams, tp.params.beams_per_batch))
+    {
+	const DedispersionKernelParams &p = tp.params;
+	int aflags = (on_gpu ? af_gpu : af_rhost) | af_zero;
+	xassert(p.nspec == 1);   // FIXME for now
+	
+	vector<long> rb_shape = { p.ringbuf_nseg * p.nt_per_segment * p.nspec };
+	vector<long> big_dshape = { p.total_beams, pow2(p.amb_rank), pow2(p.dd_rank), tp.nchunks * p.ntime };
+	vector<long> big_ishape = p.input_is_ringbuf ? rb_shape : big_dshape;
+	vector<long> big_oshape = p.output_is_ringbuf ? rb_shape : big_dshape;
+	vector<long> chunk_dshape = { p.beams_per_batch, pow2(p.amb_rank), pow2(p.dd_rank), p.ntime };
+	vector<long> chunk_istrides = on_gpu ? tp.gpu_istrides : tp.cpu_istrides;
+	vector<long> chunk_ostrides = on_gpu ? tp.gpu_ostrides : tp.cpu_ostrides;
 
-    if (tp.in_place)
-	out = in;
-    else if (tp.params.output_is_ringbuf)
-	out = out_big;
-    else
-	out = Array<void> (out_big.dtype, small_shape, ostrides, aflags);
-}
+	this->big_inbuf = Array<void> (dtype, big_ishape, aflags);
+	this->big_outbuf = Array<void> (dtype, big_oshape, aflags);
+
+	if (p.input_is_ringbuf)
+	    this->active_inbuf = big_inbuf;
+	else
+	    this->active_inbuf = Array<void> (dtype, chunk_dshape, chunk_istrides, aflags);
+	
+	if (tp.in_place)
+	    this->active_outbuf = active_inbuf;
+	else if (p.output_is_ringbuf)
+	    this->active_outbuf = big_outbuf;
+	else
+	    this->active_outbuf = Array<void> (dtype, chunk_dshape, chunk_ostrides, aflags);
+    }
+
+    void copy_input(long ichunk, long ibatch)
+    {
+	const DedispersionKernelParams &p = tp.params;
+	xassert((ichunk >= 0) && (ichunk < tp.nchunks));
+	xassert((ibatch >= 0) && (ibatch < nbatches));
+	
+	if (!p.input_is_ringbuf) {
+	    Array<void> s = big_inbuf.slice(0, ibatch * p.beams_per_batch, (ibatch+1) * p.beams_per_batch);
+	    s = s.slice(3, ichunk * p.ntime, (ichunk+1) * p.ntime);
+	    active_inbuf.fill(s);   // (slice of big_inbuf) -> (active_inbuf)
+	}
+    }
+
+    void copy_output(long ichunk, long ibatch)
+    {
+	const DedispersionKernelParams &p = tp.params;
+	xassert((ichunk >= 0) && (ichunk < tp.nchunks));
+	xassert((ibatch >= 0) && (ibatch < nbatches));
+
+	if (!p.output_is_ringbuf) {
+	    Array<void> s = big_outbuf.slice(0, ibatch * p.beams_per_batch, (ibatch+1) * p.beams_per_batch);
+	    s = s.slice(3, ichunk * p.ntime, (ichunk+1) * p.ntime);
+	    s.fill(active_outbuf);  // (active_outbuf) -> (slice of big_outbuf)
+	}
+    }
+};
 
 
-static void run_test(const TestInstanceDK &tp)
+
+static void run_test(const DedispTestInstance &tp)
 {
     const DedispersionKernelParams &p = tp.params;
     
+    long nbatches = xdiv(p.total_beams, p.beams_per_batch);
+    xassert(p.nspec == 1);  // FIXME for now
+
     cout << "\nTest GpuDedispersionKernel\n"
 	 << "    ti.params.dtype = " << p.dtype << ";\n"
 	 << "    ti.params.dd_rank = " << p.dd_rank << ";\n"
@@ -238,7 +286,6 @@ static void run_test(const TestInstanceDK &tp)
 	     << endl;
     }
 
-    xassert(p.nspec == 1);  // FIXME for now
     
     shared_ptr<ReferenceDedispersionKernel> ref_kernel = make_shared<ReferenceDedispersionKernel> (p);
     shared_ptr<GpuDedispersionKernel> gpu_kernel;
@@ -246,20 +293,17 @@ static void run_test(const TestInstanceDK &tp)
     gpu_kernel = make_shared<GpuDedispersionKernel> (p);
     gpu_kernel->allocate();
 
-    // Array allocation starts here.
-    // "Big" arrays can be either ringbufs, or "big" dedispersion bufs.
+    TestArrays cpu_arrs(tp, Dtype::native<float>(), false);  // on_gpu=false
+    TestArrays gpu_arrs(tp, tp.params.dtype, true);          // on_gpu=true
 
-    long nbatches = xdiv(p.total_beams, p.beams_per_batch);
-    vector<long> rb_shape = { p.ringbuf_nseg * p.nt_per_segment * p.nspec };
-    vector<long> dd_shape = { p.total_beams, pow2(p.amb_rank), pow2(p.dd_rank), tp.nchunks * p.ntime };
-    vector<long> big_ishape = p.input_is_ringbuf ? rb_shape : dd_shape;
-    vector<long> big_oshape = p.output_is_ringbuf ? rb_shape : dd_shape;
-
-    int iflag = tp.one_hot ? af_zero : af_random;
-    Array<float> cpu_in_big(big_ishape, af_rhost | iflag);
-    Array<float> cpu_out_big(big_oshape, af_uhost | af_zero);   // contiguous
-
-    if (tp.one_hot) {
+    if (!tp.one_hot) {
+	// Randomize (cpu_arrs.big_inbuf).
+	// FIXME ksgpu should contain a function to randomize an array.
+	Array<void> &a = cpu_arrs.big_inbuf;
+	xassert(a.is_fully_contiguous());
+	ksgpu::_randomize(a.dtype, a.data, a.size);
+    }
+    else {
 	xassert(!p.input_is_ringbuf);
 	xassert((tp.one_hot_ichunk >= 0) && (tp.one_hot_ichunk < tp.nchunks));
 	xassert((tp.one_hot_ibeam >= 0) && (tp.one_hot_ibeam < p.total_beams));
@@ -271,56 +315,30 @@ static void run_test(const TestInstanceDK &tp)
 	long a = tp.one_hot_iamb;
 	long d = tp.one_hot_iact;
 	long t = tp.one_hot_ichunk * p.ntime + tp.one_hot_itime;
-	cpu_in_big.at({b,a,d,t}) = 1.0f;
+
+	Array<float> dst = cpu_arrs.big_inbuf.cast<float>();
+	dst.at({b,a,d,t}) = 1.0f;
     }
 
-    // These are the input/output arrays for the ReferenceDedispersionKernel.
-    // They can be either "small" dedispersion bufs, or references to a "big" ringbuf.
-    Array<float> cpu_in, cpu_out;
-    _setup_io_arrays(cpu_in, cpu_out, cpu_in_big, cpu_out_big, tp, false);  // on_gpu = false
-
-    Array<void> gpu_in_big = cpu_in_big.to_gpu(tp.params.dtype);
-    Array<void> gpu_out_big(tp.params.dtype, big_oshape, af_gpu);  // contiguous
-
-    Array<void> gpu_in, gpu_out;
-    _setup_io_arrays(gpu_in, gpu_out, gpu_in_big, gpu_out_big, tp, true);  // on_gpu = true
+    // Copy (cpu_arrs.big_inbuf) -> (gpu_arrs.big_inbuf), converting dtype if necessary.
+    // FIXME ksgpu should contain a function for this.
+    Array<void> src = cpu_arrs.big_inbuf;
+    if (src.dtype != tp.params.dtype)
+	src = src.convert(tp.params.dtype);
+    gpu_arrs.big_inbuf.fill(src);
+    src = Array<void>();  // free memory
     
     for (long ichunk = 0; ichunk < tp.nchunks; ichunk++) {
 	for (long ibatch = 0; ibatch < nbatches; ibatch++) {
-	    Array<float> s;
-	    Array<void> t;
-
 	    // Reference dedispersion.
-
-	    if (!p.input_is_ringbuf) {
-		s = cpu_in_big.slice(0, ibatch * p.beams_per_batch, (ibatch+1) * p.beams_per_batch);
-		s = s.slice(3, ichunk * p.ntime, (ichunk+1) * p.ntime);
-		cpu_in.fill(s);
-	    }
-
-	    ref_kernel->apply(cpu_in, cpu_out, ibatch, ichunk);
-
-	    if (!p.output_is_ringbuf) {
-		s = cpu_out_big.slice(0, ibatch * p.beams_per_batch, (ibatch+1) * p.beams_per_batch);
-		s = s.slice(3, ichunk * p.ntime, (ichunk+1) * p.ntime);
-		s.fill(cpu_out);
-	    }
+	    cpu_arrs.copy_input(ichunk, ibatch);
+	    ref_kernel->apply(cpu_arrs.active_inbuf, cpu_arrs.active_outbuf, ibatch, ichunk);
+	    cpu_arrs.copy_output(ichunk, ibatch);
 	    
 	    // GPU dedipersion.
-
-	    if (!p.input_is_ringbuf) {
-		t = gpu_in_big.slice(0, ibatch * p.beams_per_batch, (ibatch+1) * p.beams_per_batch);
-		t = t.slice(3, ichunk * p.ntime, (ichunk+1) * p.ntime);
-		gpu_in.fill(t);
-	    }
-
-	    gpu_kernel->launch(gpu_in, gpu_out, ibatch, ichunk, nullptr);  // stream=nullptr
-
-	    if (!p.output_is_ringbuf) {
-		t = gpu_out_big.slice(0, ibatch * p.beams_per_batch, (ibatch+1) * p.beams_per_batch);
-		t = t.slice(3, ichunk * p.ntime, (ichunk+1) * p.ntime);
-		t.fill(gpu_out);
-	    }
+	    gpu_arrs.copy_input(ichunk, ibatch);
+	    gpu_kernel->launch(gpu_arrs.active_inbuf, gpu_arrs.active_outbuf, ibatch, ichunk, nullptr);  // stream=nullptr
+	    gpu_arrs.copy_output(ichunk, ibatch);
 	}
     }
     
@@ -329,9 +347,9 @@ static void run_test(const TestInstanceDK &tp)
     double epsabs = 3 * tp.params.dtype.precision() * pow(1.414, p.dd_rank);
 
     if (p.output_is_ringbuf)
-	ksgpu::assert_arrays_equal(cpu_out_big, gpu_out_big, "cpu", "gpu", {"i"}, epsabs, epsrel);
+	ksgpu::assert_arrays_equal(cpu_arrs.big_outbuf, gpu_arrs.big_outbuf, "cpu", "gpu", {"i"}, epsabs, epsrel);
     else
-	ksgpu::assert_arrays_equal(cpu_out_big, gpu_out_big, "cpu", "gpu", {"beam","amb","dmbr","time"}, epsabs, epsrel);
+	ksgpu::assert_arrays_equal(cpu_arrs.big_outbuf, gpu_arrs.big_outbuf, "cpu", "gpu", {"beam","amb","dmbr","time"}, epsabs, epsrel);
 }
 
 
@@ -339,7 +357,7 @@ void test_gpu_dedispersion_kernels()
 {
 #if 0
     // Debug
-    TestInstanceDK ti;
+    DedispTestInstance ti;
     ti.params.dtype = Dtype::native<float>();
     ti.params.dd_rank = 3;
     ti.params.amb_rank = 1;
@@ -363,7 +381,7 @@ void test_gpu_dedispersion_kernels()
 #endif    
 
 #if 1
-    TestInstanceDK ti;
+    DedispTestInstance ti;
     ti.randomize();
     run_test(ti);
 #endif
