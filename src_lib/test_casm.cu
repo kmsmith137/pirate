21c24355e6182a8d31155988c9543b6422c751e7
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2025-10-21 18:10:06 +0000

    casm checkpoint

--- src_lib/test_casm.cu
+++ src_lib/test_casm.cu
@@ -39,6 +39,15 @@ __device__ void warp_shuffle(float &x, float &y, uint bit)
 }
 
 
+template<bool Debug>
+__device__ void check_bank_conflict_free(int offset, int max_conflicts=1)
+{
+    uint m = __match_any_sync(0xffffffff, offset & 31);
+    assert(__popc(m) <= max_conflicts);
+    assert(offset >= 0);
+}
+
+
 // -------------------------------------------------------------------------------------------------
 //
 // fft_c2c (helper for "FFT1" kernel)
@@ -204,6 +213,11 @@ struct fft1_state
 
     __device__ fft1_state()
     {
+	if constexpr (Debug) {
+	    assert(blockDim.x == 32);
+	    assert(blockDim.z == 1);
+	}
+	
 	float x = 0.04908738521234052f * threadIdx.x;   // constant is (2pi)/128
 	sincosf(x, &cim, &cre);                         // note ordering (im,re)
 
@@ -215,15 +229,8 @@ struct fft1_state
 	sbase = (threadIdx.y) * 257;              // warp id
 	sbase += (threadIdx.x & 1);               // t0 <-> y0
 	sbase += __brev(threadIdx.x >> 1) >> 27;  // t4 t3 t2 t1 <-> y1 y2 y3 y4
-
-	if constexpr (Debug) {
-	    assert(blockDim.x == 32);
-	    assert(blockDim.z == 1);
-	    
-	    // Check bank conflict free
-	    uint m = __match_any_sync(0xffffffff, sbase & 31);
-	    assert(m == (1 << threadIdx.x));
-	}
+	
+	check_bank_conflict_free<Debug> (sbase);
     }
     
     __device__ void apply(float x0_re, float x0_im, float x1_re, float x1_im, float *sp)
@@ -331,24 +338,19 @@ void test_casm_fft1()
 //
 // FFT2
 //
-// There are 6 east-west feeds. It's convenient to parameterize them by
-// 0 <= fouter < 2 and 0 <= finner < 3 as follows:
+// There are 24 east-west beams 0 <= b < 24 and 6 east-west feeds 0 <= f < 6.
 //
-//   fouter=1  finner=(2,1,0)   negative feed locations
-//   fouter=0  finner=(0,1,2)   positive feed locations
+// Alternate parameterization: (bouter,binner) and (fouter,finner) where:
 //
-// Similarly, there are 24 east-west beams. It's convenient to parameterize by
-// 0 <= bouter < 2 and 0 <= binner < 12 as follows:
+//   b = bouter ? (12+binner) : (11-binner)   0 <= bouter < 2    0 <= binner < 12
+//   f = fouter ? (3+finner)  : (2-finner)    0 <= fouter < 2    0 <= finner < 3
 //
-//   bouter=1  binner=(11,...,2,1,0)   negative beam locations
-//   bouter=0  binner=(0,1,2,...11)    positive beam locations
-//
-// Thus, if we flip 'bouter' at fixed 'binner', the beam location goes to its
-// negative, and likewise for feeds.
+// This parameterization is defined so that if we flip 'bouter' at fixed 'binner',
+// the beam location goes to its negative, and likewise for feeds.
 //
 // We sometimes write 'binner' as:
 //
-//   binner = 4*b2 + 2*b1 + b0    0 <= b2 < 3   0 <= b1 < 2   0 <= b0 < 2
+//   binner = 4*b2 + 2*b1 + b0     0 <= b2 < 3    0 <= b1 < 2    0 <= b0 < 2
 //
 // NS beam locations are represented by an index 0 <= ns < 128,
 // which we sometimes decompose into its base-2 digits [ns6,...,ns0].
@@ -362,36 +364,29 @@ void test_casm_fft1()
 //
 // The I-array is distributed as follows:
 //
-//   r1 r0 <-> (bouter) (b0)
-//   t4 t3 t2 t1 t0 <-> (b1) (ns5) (ns4) (ns3) (ns2)
-//   24 warps <-> (b2) (ns6) (ns1) (ns0)
+//   r1 r0 <-> (bouter) (b1)
+//   t4 t3 t2 t1 t0 <-> (ns4) (ns3) (ns2) (ns1) (b0)
+//   24 warps <-> (b2) (ns6) (ns5) (ns0)
 //
-// The G-array shared memory layout is (note brev!)
+// The G-array shared memory layout is (where "f" is an EW feed):
 //
-//   float G[2][6][128]   strides (6*129,129,1)   bit-reversed ns
+//   float G[6][2][128];  // (f,reim,ns), strides (257,128,1)
 //
 // We read from the G-array in register assignment:
 //
-//   t4 t3 t2 t1 t0 <-> (fouter) (ns5) (ns4) (ns3) (ns2)
+//   t4 t3 t2 t1 t0 <-> (ns4) (ns3) (ns2) (ns1) (fouter)
 //
 // This is bank conflict free, since flipping 'fouter' always produces
-// an odd change in f, the f-stride is odd (129), and the (ns5, ..., ns2)
-// strides are (2, 4, 8, 16) after bit reversal.
+// an odd change in f, and the f-stride is odd (257).
 //
-// The I-array shared memory layout is:
+// The I-array shared memory layout is (where "b" indexes an EW beam):
 //
-//   float I[24][128]   strides (133,1)
+//   float I[24][128];   // (b,ns), strides (133,1)
 //
-// We write to the I-array in register assignment:
+// We write to the I-array in the bank conflict free register assignment:
 //
-//   t4 t3 t2 t1 t0 <-> (b1) (bouter) (ns4) (ns3) (ns2)
-//
-// This is bank conflict free, since flipping 'bouter' always produces
-// an odd change in b, the b-stride is odd (133), and the (ns4, ns3, ns2)
-// strides are (16, 8, 4).
+//   t4 t3 t2 t1 t0 <-> (ns4) (ns3) (ns2) (ns1) (b0)
 
-
-#if 0
 
 template<bool Debug>
 struct fft2_state
@@ -400,13 +395,16 @@ struct fft2_state
     // phases, but the number of distinct phases is 24/warp or 72/block.
     // Maybe better to distribute registers as needed with __shfl_sync()?
      
-    float I[2][2];      // beams are indexed by (bouter, binner)
-    float pcos[2][3];   // beamforming phases are indexed by (b0, finner)
-    float psin[2][3];   // beamforming phases are indexed by (b0, finner)
+    float I[2][2];      // beams are indexed by (bouter, b1)
+    float pcos[2][3];   // beamforming phases are indexed by (b1, finner)
+    float psin[2][3];   // beamforming phases are indexed by (b1, finner)
     
-    int soff_g;    // base shared memory offset in G-array.
-    int soff_i;    // base shared memory offset in I-array.
-    
+    int soff_g;     // base shared memory offset in G-array.
+    int soff_i0;    // base shared memory offset in I-array, bouter=0
+    int soff_i1;    // base shared memory offset in I-array, bouter=1
+
+
+    // FIXME is alpha[3] the best interface here?
     __device__ fft2_state(float alpha[3])
     {
 	if constexpr (Debug) {
@@ -417,124 +415,241 @@ struct fft2_state
 	
 	I[0][0] = I[0][1] = I[1][0] = I[1][1] = 0.0f;
 
-	// Each warp maps to an (b2, ns6, ns01) triple.
-	uint b2 = (threadIdx.y >> 3);       // 0 <= b2 < 3
-	uint ns6 = (threadIdx.y >> 2) & 1;  // 0 <= ns6 < 2
-	uint ns01 = (threadIdx.y & 3);      // 0 <= ns01 < 4
+	// Each warp maps to an (b2, ns56, ns0) triple.
+	uint ns0 = (threadIdx.y & 1);        // 0 <= ns0 < 2
+	uint ns56 = (threadIdx.y >> 1) & 3;  // 0 <= ns56 < 4
+	uint b2 = (threadIdx.y >> 3);        // 0 <= b2 < 3
 
-	// Each thread maps to a (b1, ns25) pair.
-	uint b1 = (threadIdx.x & 1);
-	uint ns25 = (threadIdx.x >> 1);
+	// Each thread maps to a (ns14, b0) pair.
+	uint b0 = (threadIdx.x & 1);
+	uint ns14 = (threadIdx.x >> 1);
 
-	// Beamforming phases are indexed by (b0, finner)
+	// Beamforming phases are indexed by (b1, finner)
 	#pragma unroll
-	for (uint b0 = 0; b0 < 2; b0++) {
+	for (uint b1 = 0; b1 < 2; b1++) {
 	    // Beamforming phase is exp(i*t*alpha[finner]) where t = 1+2*binner
 	    float t = (b2 << 3) | (b1 << 2) | (b0 << 1) | 1;
 	    
 	    #pragma unroll
 	    for (uint finner = 0; finner < 3; finner++)
-		sincosf(t * alpha[finner], psin[b0][finner], pcos[b0][finner]);
+		sincosf(t * alpha[finner], &psin[b1][finner], &pcos[b1][finner]);
 	}
 
 	// Shared memory offset for reading G-array:
 	//
-	//   float G[2][6][128]   strides (6*129,129,1)   bit-reversed ns
+	//   float G[6][2][128];  // (f,reim,ns), strides (257,128,1)
 	//
 	// When we read from the G-array, we read it as:
 	//
-	//   t0 t1 t2 t3 t4 <-> (fouter) (ns5) (ns4) (ns3) (ns2)
+	//   t4 t3 t2 t1 t0 <-> (ns4) (ns3) (ns2) (ns1) (fouter)
 	//
-	// 'soff_g' uses per-warp values of (ns01,ns6), and (finner,ReIm)=(0,0).
+	// 'soff_g' is the offset assuming finner=reim=0.
 	
 	uint fouter = threadIdx.x & 1;
-	uint ns = (ns6 << 6) | (ns25 << 2) | ns01;
-	soff_g = (__brev(ns) >> 25) + (fouter ? (5*129) : 0);
+	uint ns = (ns56 << 5) | (ns14 << 1) | ns0;
+	soff_g = (fouter ? (2*257) : (3*257)) + ns;
 
 	// Shared memory offset for writing I-array:
 	//
-	//   float I[24][128]   strides (133,1)
+	//   float I[24][128];   // (b,ns), strides (133,1)
 	//
 	// When we write to the I-array, we write as
 	//
-	//   t0 t1 t2 t3 t4 <-> (b1) (bouter) (ns4) (ns3) (ns2)
+	//   t4 t3 t2 t1 t0 <-> (ns4) (ns3) (ns2) (ns1) (b0)
 	//
-	// 'soff_i' uses        
+	// 'soff_i{0,1}' is the offset with bouter={0,1} and b1=0.
+
+	uint binner = (b2 << 2) | b0;
+	soff_i0 = 133*(12+binner) + ns;  // offset for bouter=0
+	soff_i1 = 133*(11-binner) + ns;  // offset for binner=1
+
+	check_bank_conflict_free<Debug> (soff_i0);
+	check_bank_conflict_free<Debug> (soff_i1);
     }
 
+    
     // Accumulates one (time,pol) into I-registers.
-    __device__ void step(const float *sp)
+    __device__ void apply(const float *sp)
     {
-	float Fre[2][2];   // (b1, bflip)
-	float Fim[2][2];   // (b1, bflip)
+	// Beamformed electric fields are accumulated here.
+	float Fre[2][2];   // (bouter, b1)
+	float Fim[2][2];   // (bouter, b1)
 
 	Fre[0][0] = Fre[0][1] = Fre[1][0] = Fre[1][1] = 0.0f;
 	Fim[0][0] = Fim[0][1] = Fim[1][0] = Fim[1][1] = 0.0f;
 
-	int ds = (threadIdx.x & 1) ? (-129) : 129;
+	// finner-stride in the G-array
+	int fouter = threadIdx.x & 1;
+	int ds = fouter ? (-257) : 257;
 	
         #pragma unroll
-	for (int f1 = 0; f1 < 3; f1++) {
-	    float Gre[2];  // fflip
-	    float Gim[2];  // fflip
-
-	    // FIXME loading logic could be improved.
-	    int s = soff_g + f1*ds;
+	for (int finner = 0; finner < 3; finner++) {
+	    int s = soff_g + finner*ds;
+	    check_bank_conflict_free<Debug> (s);
+	    
 	    float tre = sp[s];
-	    float tim = sp[s + 6*129];
-	    Gre[0] = __shfl_sync(0xffffffff, tre, threadIdx.x & ~1);
-	    Gre[1] = __shfl_sync(0xffffffff, tre, threadIdx.x | 1);
-	    Gim[0] = __shfl_sync(0xffffffff, tim, threadIdx.x & ~1);
-	    Gim[1] = __shfl_sync(0xffffffff, tim, threadIdx.x | 1);
+	    float tim = sp[s + 128];
+
+	    // FIXME can be improved.
+	    // u{0,1} index is fouter.
+	    float u0_re = __shfl_sync(0xffffffff, tre, threadIdx.x & ~1);
+	    float u1_re = __shfl_sync(0xffffffff, tre, threadIdx.x | 1);
+	    float u0_im = __shfl_sync(0xffffffff, tim, threadIdx.x & ~1);
+	    float u1_im = __shfl_sync(0xffffffff, tim, threadIdx.x | 1);
 
 	    #pragma unroll
 	    for (int b1 = 0; b1 < 2; b1++) {
-		// FIXME can be sped up with FFT-style tricks.
+		// FIXME can be sped up with FFT-style trick.
 
-		// F[0] += (phase) (G[0])
-		// F[0] += (phase^*) (G[1])
-		// F[1] += (phase^*) (G[0])
-		// F[1] += (phase) (G[1])
-
+		// F[0][b1] += (phase) (u0)
+		// F[0][b1] += (phase^*) (u1)
+		// F[1][b1] += (phase^*) (u0)
+		// F[1][b1] += (phase) (u1)
+		
 		// FIXME I don't think zma() will be called here, in the final kernel.
-		zma(Fre[b1][0], Fim[b1][0], pcos[b1][f1],  psin[b1][f1], Gre[0], Gim[0]);
-		zma(Fre[b1][0], Fim[b1][0], pcos[b1][f1], -psin[b1][f1], Gre[1], Gim[1]);
-		zma(Fre[b1][1], Fim[b1][1], pcos[b1][f1], -psin[b1][f1], Gre[0], Gim[0]);
-		zma(Fre[b1][1], Fim[b1][1], pcos[b1][f1],  psin[b1][f1], Gre[1], Gim[1]);
+		zma(Fre[0][b1], Fim[0][b1], pcos[b1][finner],  psin[b1][finner], u0_re, u0_im);
+		zma(Fre[0][b1], Fim[0][b1], pcos[b1][finner], -psin[b1][finner], u1_re, u1_im);  // note (-psin)
+		zma(Fre[1][b1], Fim[1][b1], pcos[b1][finner], -psin[b1][finner], u0_re, u0_im);  // note (-psin)
+		zma(Fre[1][b1], Fim[1][b1], pcos[b1][finner],  psin[b1][finner], u1_re, u1_im);
 	    }
 	}
 
 	#pragma unroll
-	for (int b1 = 0; b1 < 2; b1++) {
+	for (int bouter = 0; bouter < 2; bouter++) {
 	    #pragma unroll
-	    for (int bflip = 0; bflip < 2; bflip++)
-		I[b1][bflip] += (Fre[b1][bflip] * Fre[b1][bflip]) + (Fim[b1][bflip] * Fim[b1][bflip]);
+	    for (int b1 = 0; b1 < 2; b1++)
+		I[bouter][b1] += (Fre[bouter][b1] * Fre[bouter][b1]) + (Fim[bouter][b1] * Fim[bouter][b1]);
 	}
     }
 
-    // Writes to shared memory.
-    __device__ void write(float *sp)
+    // Writes I[] register to shared memory and zeroes the registers.
+    __device__ void write_and_reset(float *sp)
     {
-	// r1 r0 <-> (bouter) (b0)
-	// t4 t3 t2 t1 t0 <-> (b1) (ns5) (ns4) (ns3) (ns2)
+	// Beams are indexed by (bouter, b1).
+	//   float I[24][128];   // (b,ns), strides (133,1)
 
-	xxx;  // warp shuffle
+	sp[soff_i0] = I[0][0];
+	sp[soff_i1] = I[1][0];
 	
-	// r1 r0 <-> (ns5) (b0)
-	// t4 t3 t2 t1 t0 <-> (b1) (bouter) (ns4) (ns3) (ns2)
+	sp[soff_i0 + 2*133] = I[0][1];
+	sp[soff_i1 - 2*133] = I[1][1];
 
-	// ns5_stride = 32, b0_stride = 133
-	sp[soff_i] = I[0][0];
-	sp[soff_i + 133] = I[0][1];
-	sp[soff_i + 32] = I[1][0];
-	sp[soff_i + 32+133] = I[1][1];
-
-	// Reset
 	I[0][0] = I[0][1] = I[1][0] = I[1][1] = 0.0f;
     }
 };
 
-#endif  // FFT2
+
+// float G[TP][6][128][2];
+// float I[24][128];
+// Launch with {32,24,1} threads.
+
+__global__ void fft2_test_kernel(const float *gp, float *ip, int TP, const float *alpha)
+{
+    __shared__ float shmem_g[6*257];   // size (6,2,128), strides (257,128,1)
+    __shared__ float shmem_i[24*133];  // size (24,128), strides (133,1)
+
+    assert(blockDim.x == 32);
+    assert(blockDim.y == 24);
+    assert(blockDim.z == 1);
+
+    float a[3];
+    a[0] = alpha[0];
+    a[1] = alpha[1];
+    a[2] = alpha[2];
+    
+    fft2_state<true> fft2(a);  // Debug=true
+    
+    // Set up G-array copy (global) -> (shared)
+    int gns = ((threadIdx.y & 3) << 5) + threadIdx.x;
+    int gew = (threadIdx.y >> 2);
+    int gsh = (257*gew) + gns;   // array offset in shmem_g[] array
+    float2 *gp2 = (float2 *)(gp) + 128*gew + gns;  // per-(warp+thread) offsets applied
+    
+    for (int tp = 0; tp < TP; tp++) {
+	// G-array copy (global) -> (shared)
+	float2 g = *gp2;
+	shmem_g[gsh] = g.x;      // real part
+	shmem_g[gsh+128] = g.y;  // imag part
+	gp2 += 6*128;
+
+	__syncthreads();
+
+	fft2.apply(shmem_g);
+
+	__syncthreads();
+    }
+
+    fft2.write_and_reset(shmem_i);
+    __syncthreads();
+	
+    // Set up I-array copy (shared) -> (global)
+    int goff = 133*threadIdx.y + threadIdx.x;  // array offset in ip[] global array
+    int soff = 128*threadIdx.y + threadIdx.x;  // array offset in shmem_i[] shared array
+    
+    for (int j = 0; j < 4; j++)
+	ip[goff + 32*j] = shmem_i[soff + 32*j];
+}
+
+
+// float G[TP][6][128][2];
+// float I[24][128];
+
+void fft2_reference_kernel(const float *gp, float *ip, int TP, const float *alpha)
+{
+    // beamforming phase is exp(i * bloc[b] * floc[f])
+    float bloc[24];
+    float floc[6];
+
+    for (int binner = 0; binner < 12; binner++) {
+	bloc[12+binner] = 1 + 2*binner;
+	bloc[11-binner] = -(1 + 2*binner);
+    }
+
+    for (int finner = 0; finner < 3; finner++) {
+	floc[3+finner] = alpha[finner];
+	floc[2-finner] = -alpha[finner];
+    }
+    
+    for (int ns = 0; ns < 128; ns++) {
+	for (int b = 0; b < 24; b++) {
+	    float I = 0.0f;
+
+	    for (int f = 0; f < 6; f++) {
+		float xre = cosf(bloc[b] * floc[f]);
+		float xim = sinf(bloc[b] * floc[f]);
+		float yre = gp[256*f + 2*ns];
+		float yim = gp[256*f + 2*ns + 1];
+		float zre = xre*yre - xim*yim;
+		float zim = xre*yim + xim*yre;
+
+		I += (zre*zre + zim*zim);
+	    }
+
+	    ip[128*b + ns] = I;
+	}
+    }
+}
+
+
+void test_casm_fft2()
+{
+    int TP = 4;
+    Array<float> g({TP,6,128,2}, af_rhost | af_random);
+    Array<float> i_cpu({24,128}, af_rhost | af_random);
+    Array<float> i_gpu({24,128}, af_gpu | af_random);
+    Array<float> alpha({3}, af_rhost | af_random);
+    
+    fft2_reference_kernel(g.data, i_cpu.data, TP, alpha.data);
+    
+    g = g.to_gpu();
+    alpha = alpha.to_gpu();
+    
+    fft2_test_kernel<<< 1, {32,24,1} >>> (g.data, i_gpu.data, TP, alpha.data);
+    CUDA_PEEK("fft2_test_kernel");
+
+    assert_arrays_equal(i_cpu, i_gpu, "cpu", "gpu", {"b","ns"}, 1.0e-4);
+    cout << "test_casm_fft2: pass" << endl;
+}
 
 
 // -------------------------------------------------------------------------------------------------
@@ -636,15 +751,10 @@ __device__ float interpolate_fast(const float *sp, float x, float y)
 	int src_lane = (threadIdx.x & 0x10) | iouter;
 	
 	int s = __shfl_sync(0xffffffff, sg, src_lane) + ds;
+	check_bank_conflict_free<Debug> (s, 2);   // at most 2:1 bank conflict
+	
 	float dx = __shfl_sync(0xffffffff, dx_g, src_lane);
 	float dy = __shfl_sync(0xffffffff, dy_g, src_lane);
-
-	if constexpr (Debug) {
-	    assert(s >= 0);
-	    assert(s < 24*133);
-	    uint m = __match_any_sync(0xffffffff, s & 31);
-	    assert(__popc(m) <= 2);  // at most 2:1 bank conflict
-	}
 	
 	float w = pfx * (dx+ax) * (dx+bx) * (dx+cx) * (dy+ay) * (dy+by) * (dy+cy);
 	float t = w * sp[s];
@@ -711,6 +821,7 @@ void test_casm()
 {
     test_casm_fft_c2c();
     test_casm_fft1();
+    test_casm_fft2();
     test_casm_interpolation();
 }
 
