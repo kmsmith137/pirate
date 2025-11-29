c6cc32f20a2d4d1f0f258e2e3ff5b9aeb7b3b0bf
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2025-11-29 22:35:05 +0000

    oops, forgot this in previous commit

---
+++ include/pirate/loose_ends/tests.hpp
@@ -0,0 +1,20 @@
+#ifndef _PIRATE_LOOSE_ENDS_TESTS_HPP
+#define _PIRATE_LOOSE_ENDS_TESTS_HPP
+
+namespace pirate {
+#if 0
+}  // editor auto-indent
+#endif
+
+
+// "Zombie" tests (code that I wrote during protoyping that may never get used)
+extern void test_avx2_m64_outbuf();
+extern void test_cpu_downsampler();
+extern void test_gpu_downsample();
+extern void test_gpu_transpose();
+extern void test_gpu_reduce2();
+
+
+}  // namespace pirate
+
+#endif // _PIRATE_LOOSE_ENDS_TESTS_HPP
---
+++ include/pirate/loose_ends/timing.hpp
@@ -0,0 +1,23 @@
+#ifndef _PIRATE_LOOSE_ENDS_TIMING_HPP
+#define _PIRATE_LOOSE_ENDS_TIMING_HPP
+
+namespace pirate {
+#if 0
+}  // editor auto-indent
+#endif
+
+
+// time_cpu_downsample.cu
+extern void time_cpu_downsample(int nthreads);
+
+// time_gpu_downsample.cu
+extern void time_gpu_downsample();
+
+// time_gpu_transpose.cu
+extern void time_gpu_transpose();
+
+    
+}  // namespace pirate
+
+#endif // _PIRATE_TIMING_LOOSE_ENDS_HPP
+
