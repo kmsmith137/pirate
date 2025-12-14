e19713c9625c5f19f9e51088d64560d91cb563e5
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2025-12-14 20:22:37 +0000

    delete ReferenceTree (old code), now using ReferenceTreeWithSubbands (new code) everywhere

--- include/pirate/ReferenceTree.hpp
+++ include/pirate/ReferenceTree.hpp
@@ -14,54 +14,14 @@ namespace pirate {
 #endif
 
 
-// ReferenceTree: simple, self-contained reference implementation of tree dedispersion.
+// ReferenceTree: reference implementation of tree dedispersion.
 //
-// Processes input incrementally in chunks of shape (..., nfreq, ntime), where "..."
-// represents an arbitrary number of spectator axes. The time axis must be contiguous.
-// The number of frequencies must be a power of two.
+// Processes input incrementally in chunks of shape 
+//   (nbeams, pow2(amb_rank), pow2(dd_rank), ntime * nspec).
 //
 // The RefrerenceTree is unaware of the larger dedispersion plan (stage1/stage2 split,
 // early triggers, downsampling, etc.) but can be used as a "building block" to implement
 // these features.
-
-
-class ReferenceTree
-{
-public:
-    // Frequency axis is second-to-last, inner (ntime*nspec) axis is last.
-    // All other indices are spectators.
-    ReferenceTree(const std::vector<long> &shape, long nspec);
-    ReferenceTree(int ndim, const long *shape, long nspec);
-    
-    // Dedispersion is done in-place.
-    // Array shape is (Spectator indices) + (nfreq, ntime*nspec).
-    void dedisperse(ksgpu::Array<float> &arr);
-
-    // Morally equivalent to make_shared<ReferenceTree> (...).
-    // (Necessary since make_shared doesn't seem to work with initializer lists.)
-    static std::shared_ptr<ReferenceTree> make(std::initializer_list<long> shape, long nspec);
-    
-protected:
-    // (Spectator indices) + (nfreq, ntime*nspec)
-    std::vector<long> shape;
-
-    int ndim = -1;
-    int rank = -1;
-    long nfreq = -1;
-    long ntime = -1;
-    long nspec = -1;
-    long ninner = -1;   // = ntime*nspec
-    long npstate = 0;
-    
-    ksgpu::Array<float> pstate;          // can be large
-    ksgpu::Array<float> scratch;         // always small (length (ntime+1)*nspec)
-};
-
-
-// -------------------------------------------------------------------------------------------------
-//
-// FIXME: currently, 'struct ReferenceTree' and 'struct ReferenceTreeWithSubbands' are independent
-// classes. They should be combined into one class (or at least share a base class).
 
 
 struct ReferenceTreeWithSubbands
--- src_lib/ReferenceTree.cu
+++ src_lib/ReferenceTree.cu
@@ -15,132 +15,6 @@ namespace pirate {
 #endif
 
 
-// Assumes array has been transposed so that shape is (spectator_indices, nfreq, ntime*nspec)
-static float *_dedisperse(float *arr, int ndim, const long *shape, const long *strides, int rank, long ntime, long nspec, float *rp, float *sp)
-{
-    // Recursively reduce to the case ndim == 2.
-    if (ndim > 2) {
-        for (long i = 0; i < shape[0]; i++)
-            rp = _dedisperse(arr + i*strides[0], ndim-1, shape+1, strides+1, rank, ntime, nspec, rp, sp);
-        return rp;
-    }
-
-    xassert(ndim == 2);    
-    xassert(shape[0] == pow2(rank));
-    xassert(shape[1] == ntime * nspec);
-    xassert((shape[1] == 1) || (strides[1] == 1));
-
-    long ninner = ntime * nspec;
-    long rstride = strides[0];
-    
-    for (int r = 0; r < rank; r++) {
-        long ni = pow2(rank-r-1);
-        long nj = pow2(r);
-
-        // The index 'i' represents a coarse frequency.
-        // The index 'j' represents a bit-reversed delay.
-        
-        for (long i = 0; i < ni; i++) {
-            for (long j = 0; j < nj; j++) {
-                long row0 = i*(2*nj) + j;
-                long row1 = row0 + nj;
-                
-                float *a0 = arr + (row0 * rstride);
-                float *a1 = arr + (row1 * rstride);
-
-                // FIXME precompute these!!!
-                long lag1 = bit_reverse_slow(j,r) * nspec;
-                long lag0 = lag1 + nspec;
-
-                // Fill 'scratch' with n=(ntime+1)*nspec samples, obtained by applying lag0.
-                long n = (ntime+1) * nspec;   // total samples in scratch buffer
-                long n0 = min(lag0, n);       // number of samples which come from ring buffer
-                long n1 = n - n0;             // number of samples which come from 'a0'
-                memcpy(sp, rp, n0 * sizeof(float));
-                memcpy(sp+n0, a0, n1 * sizeof(float));
-
-                // Advance ring buffer (length lag0) by ninner samples.
-                long m0 = max(lag0-ninner, 0L);  // number of samples which stay in ring buffer
-                long m1 = lag0 - m0;             // number of samples which come from 'a0'
-                memmove(rp, rp+ninner, m0 * sizeof(float));
-                memcpy(rp+m0, a0+ninner-m1, m1 * sizeof(float));
-                rp += lag0;
-
-                // Apply dedispersion (sp,a1) -> (a0,a1).
-                for (long k = 0; k < ninner; k++) {
-                    float x0 = sp[k];
-                    float x1 = sp[k+nspec];
-                    float y = a1[k];
-                    a0[k] = x1 + y;
-                    a1[k] = x0 + y;
-                }
-            }           
-        }
-    }
-
-    return rp;
-}
-
-
-ReferenceTree::ReferenceTree(const std::vector<long> &shape_, long nspec_) :
-    ReferenceTree(shape_.size(), &shape_[0], nspec_)
-{ }
-
-
-ReferenceTree::ReferenceTree(int ndim_, const long *shape_, long nspec_) :
-    ndim(ndim_), nspec(nspec_)
-{
-    xassert(ndim >= 2);
-    xassert(nspec > 0);
-    xassert_divisible(shape_[ndim-1], nspec);
-    
-    this->shape.resize(ndim);
-
-    for (int d = 0; d < ndim; d++) {
-        shape[d] = shape_[d];
-        xassert(shape[d] > 0);
-    }
-    
-    this->nfreq = shape[ndim-2];
-    this->ntime = xdiv(shape[ndim-1], nspec);
-    
-    xassert(is_power_of_two(nfreq));
-    this->rank = integer_log2(nfreq);
-
-    this->npstate = rstate_len(rank) * nspec;   // rstate_len() is declared in utils.hpp
-    for (int d = 0; d < ndim-2; d++)
-        npstate *= shape[d];
-    
-    this->pstate = Array<float> ({npstate}, af_uhost | af_zero);
-    this->scratch = Array<float> ({(ntime+1)*nspec}, af_uhost | af_zero);
-}
-
-
-void ReferenceTree::dedisperse(ksgpu::Array<float> &arr)
-{
-    xassert(arr.on_host());
-    xassert(arr.shape_equals(this->shape));
-    xassert((ntime == 1) || (arr.strides[ndim-1] == 1));
-        
-    if (rank > 0) {
-        float *rp_end = _dedisperse(arr.data, ndim, arr.shape, arr.strides, rank, ntime, nspec, pstate.data, scratch.data);
-        xassert(rp_end == pstate.data + npstate);
-    }
-}    
-
-
-// static member function
-shared_ptr<ReferenceTree> ReferenceTree::make(std::initializer_list<long> shape, long nspec_)
-{
-    return make_shared<ReferenceTree> (shape.size(), shape.begin(), nspec_);
-}
-
-
-// -------------------------------------------------------------------------------------------------
-//
-// ReferenceTreeWithSubbands
-
-
 ReferenceTreeWithSubbands::ReferenceTreeWithSubbands(const Params &params_) :
     params(params_), fs(params_.subband_counts)
 {
@@ -520,16 +394,25 @@ void ReferenceTreeWithSubbands::test()
 
     ReferenceTreeWithSubbands tree_with_subbands(params);
 
-    std::vector<std::shared_ptr<ReferenceTree>> subtrees(fs.F);
+    std::vector<std::shared_ptr<ReferenceTreeWithSubbands>> subtrees(F);
 
     // Initialize subtrees.
     for (long f = 0; f < F; f++) {
         long ilo = fs.f_to_ilo.at(f) << (dd_rank - pf_rank);
         long ihi = fs.f_to_ihi.at(f) << (dd_rank - pf_rank);
+
         long subtree_size = ihi - ilo;
+        long subtree_rank = integer_log2(subtree_size);
        
-        std::initializer_list<long> shape = { B, A, subtree_size, T*S };
-        subtrees[f] = ReferenceTree::make(shape, S);
+        Params subtree_params;
+        subtree_params.num_beams = B;
+        subtree_params.amb_rank = amb_rank;
+        subtree_params.dd_rank = subtree_rank;
+        subtree_params.ntime = T;
+        subtree_params.nspec = S;
+        subtree_params.subband_counts = {1};
+
+        subtrees[f] = make_shared<ReferenceTreeWithSubbands> (subtree_params);
     }
 
     // FIXME should test strides! ('buf' and 'out' only)
@@ -540,6 +423,7 @@ void ReferenceTreeWithSubbands::test()
     // For subtrees.
     Array<float> buf2({B,A,Din,T*S}, af_uhost | af_zero);
     Array<float> out2({B,Dpf,M,T*S}, af_uhost | af_zero);
+    Array<float> sb_empty;
 
     for (long ichunk = 0; ichunk < nchunks; ichunk++) {
         in.randomize();
@@ -564,7 +448,7 @@ void ReferenceTreeWithSubbands::test()
             Array<float> src = in.slice(2, ilo, ihi);
             dd.fill(src);
 
-            subtrees[f]->dedisperse(dd);
+            subtrees[f]->dedisperse(dd, sb_empty);
 
             // Copy dd -> out2.
             // The ambient index 'a' is a bit-reversed coarse DM.
