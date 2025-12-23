6cd5c48df376c8b6d046e19feff4eabce11555bb
Kendrick Smith <kmsmith137@gmail.com>
2025-12-23 04:15:50 +0000

    cosmetic changes to DedispersionPlan.hpp

--- include/pirate/DedispersionPlan.hpp
+++ include/pirate/DedispersionPlan.hpp
@@ -8,6 +8,7 @@
 #include "RingbufCopyKernel.hpp"         // struct RingbufCopyKernelParams
 #include "TreeGriddingKernel.hpp"        // struct TreeGriddingKernelParams
 #include "PeakFindingKernel.hpp"         // struct PeakFindingKernelParams
+#include "MegaRingbuf.hpp"               // struct MegaRingbuf
 
 #include <vector>
 #include <memory>  // shared_ptr
@@ -18,9 +19,6 @@ namespace pirate {
 #if 0
 }  // editor auto-indent
 #endif
-
-// Defined in MegaRingbuf.hpp.
-struct MegaRingbuf;
 
 
 struct DedispersionPlan
@@ -34,35 +32,22 @@ struct DedispersionPlan
     // plus one "early" tree for each EarlyTrigger (see DedispersionConfig.hpp). Thus,
     // there is a many-to-one mapping from stage2 trees to stage1 trees.
 
-    long stage1_ntrees = 0;  // same as config.num_downsampling_levels
-    long stage2_ntrees = 0;
+    long stage1_ntrees = 0;  // = (config.num_downsampling_levels)
+    long stage2_ntrees = 0;  // = (config.num_downsampling_levels + total number of early triggers)
 
     // Defines the mapping from stage2 trees to stage1 trees.
     // Length (stage2_ntrees), each element is an index 0 <= ds_level < stage1_ntrees.
     std::vector<long> stage2_ds_level;
 
-    TreeGriddingKernelParams tree_gridding_kernel_params;
-    
-    DedispersionBufferParams stage1_dd_buf_params;  // (number of buffers) = stage1_ntrees
-    DedispersionBufferParams stage2_dd_buf_params;  // (number of buffers) = stage2_ntrees
-    
-    std::vector<DedispersionKernelParams> stage1_dd_kernel_params;  // length stage1_ntrees
-    std::vector<DedispersionKernelParams> stage2_dd_kernel_params;  // length stage2_ntrees
-    std::vector<PeakFindingKernelParams> stage2_pf_params;          // length stage2_ntrees
-    
-    LaggedDownsamplingKernelParams lds_params;
-
-    // Only needed if early triggers are used.
-    RingbufCopyKernelParams g2g_copy_kernel_params;
-    RingbufCopyKernelParams h2h_copy_kernel_params;
+    int nelts_per_segment = 0;   // currently always constants::bytes_per_gpu_cache_line / (sizeof config dtype)
+    int nbytes_per_segment = 0;  // currently always constants::bytes_per_gpu_cache_line
 
     void print(std::ostream &os=std::cout, int indent=0) const;
-    
-    // Members after this should not be needed "from the outside".
-    // FIXME: reorganize code to reflect this?
-    
-    // --------------------  Helper classes  --------------------
-    
+
+    // -------------------------------------------------------------------------------------------------
+    //
+    // Stage1Trees, Stage2Trees.
+
     struct Stage1Tree
     {
         // Note: total tree rank (dd_rank + amb_rank) is equal to (config.tree_rank - (ds_level ? 1 : 0)).        
@@ -80,14 +65,31 @@ struct DedispersionPlan
         int early_dd_rank = 0;   // Active rank of this Stage2Tree (always <= pri_dd_rank)
         int nt_ds = 0;           // Downsampled time samples per chunk (= config.time_samples_per_chunk / pow2(ds_level))
     };
-
-    int nelts_per_segment = 0;   // currently always constants::bytes_per_gpu_cache_line / (sizeof config dtype)
-    int nbytes_per_segment = 0;  // currently always constants::bytes_per_gpu_cache_line
     
     std::vector<Stage1Tree> stage1_trees;  // length stage1_ntrees
     std::vector<Stage2Tree> stage2_trees;  // length stage2_ntrees
 
+    // -------------------------------------------------------------------------------------------------
+    //
+    // Low-level data needed for compute kernels.
+
+    // MegaRingbuf: this data structure is the "nerve center" of the real-time FRB search.
+    // I have written a short novel explaining how it works, in MegaRingbuf.hpp.
     std::shared_ptr<MegaRingbuf> mega_ringbuf;
+
+    TreeGriddingKernelParams tree_gridding_kernel_params;
+    LaggedDownsamplingKernelParams lds_params;
+
+    DedispersionBufferParams stage1_dd_buf_params;  // (number of buffers) = stage1_ntrees
+    DedispersionBufferParams stage2_dd_buf_params;  // (number of buffers) = stage2_ntrees
+    
+    std::vector<DedispersionKernelParams> stage1_dd_kernel_params;  // length stage1_ntrees
+    std::vector<DedispersionKernelParams> stage2_dd_kernel_params;  // length stage2_ntrees
+    std::vector<PeakFindingKernelParams> stage2_pf_params;          // length stage2_ntrees
+
+    // Only needed if early triggers are used.
+    RingbufCopyKernelParams g2g_copy_kernel_params;
+    RingbufCopyKernelParams h2h_copy_kernel_params;
 };
 
 
