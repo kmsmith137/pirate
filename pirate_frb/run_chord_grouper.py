2d266ebf8c4e98cc69ec7f70bd333e7d0024d2dc
Dustin Lang <dstndstn@gmail.com>
2026-06-11 19:53:09 +0000

    compute FPGAcounts time ranges for chunks

--- pirate_frb/run_chord_grouper.py
+++ pirate_frb/run_chord_grouper.py
@@ -25,6 +25,11 @@ def _run_chord_grouper(grouper_addr, sifter_addr, grouper, delay=0.0):
     from .rpc.grpc.frb_sifter_pb2 import ConfigMessage, FrbEventsMessage, FrbEvent
     import grpc
     import yaml
+    try:
+        from yaml import CLoader as Loader, CDumper as Dumper
+    except ImportError:
+        from yaml import Loader, Dumper
+    from datetime import datetime
 
     print('Starting CHORD grouper: sifter address is', sifter_addr)
 
@@ -47,17 +52,32 @@ def _run_chord_grouper(grouper_addr, sifter_addr, grouper, delay=0.0):
     # time_samples_per_chunk
     #grouper.nt_in
 
+    xengine = yaml.load(grouper.xengine_metadata_yaml_string, Loader=Loader)
+    beamset = xengine['beamset']
+    print('Beamset:', beamset)
+
+    # timing
+    seq_per_frb_time_sample = xengine['seq_per_frb_time_sample']
+    time_samples_per_chunk = grouper.nt_in
+    fpga0_nano = xengine['unix_ns_at_seq_0']
+    nano_per_fpga = xengine['dt_ns_per_seq']
+    fpga_per_chunk = seq_per_frb_time_sample * time_samples_per_chunk
+    print('Time samples per chunk:', time_samples_per_chunk)
+    print('FPGA seq per time sample:', seq_per_frb_time_sample)
+    print('nanoseconds per FPGA seq:', nano_per_fpga)
+    print('FPGA per chunk:', fpga_per_chunk)
+
     my_config = dict(the_answer=42)
     my_config_yaml = yaml.dump(my_config)
 
     ch1 = grpc.insecure_channel(sifter_addr)
-    stub1 = FrbSifterStub(ch1)
+    sifter = FrbSifterStub(ch1)
     msg = ConfigMessage(xengine_yaml=grouper.xengine_metadata_yaml_string,
                         pirate_yaml=grouper.dedispersion_config_yaml_string,
                         dedispersion_plan_yaml=grouper.dedispersion_plan_yaml_string,
                         grouper_yaml=my_config_yaml)
     print('Sending first gRPC to Sifter...')
-    r1 = stub1.CheckConfiguration(msg)
+    r1 = sifter.CheckConfiguration(msg)
     print('Got Sifter result:', r1.ok)
 
     for ichunk in itertools.count():            # loop over time chunks
@@ -78,6 +98,26 @@ def _run_chord_grouper(grouper_addr, sifter_addr, grouper, delay=0.0):
                 # outputs.out_max (float16/float32)
                 #   each out_max has shape (beam_per_batch, DM, T)
 
+                #print('outputs:', dir(outputs))
+                #'ibeam', 'ichunk_fpga_based', 'ichunk_zero_based'
+                if ibatch == 0:
+                    print('output: ibeam', outputs.ibeam,
+                          'ichunk_fpga:', outputs.ichunk_fpga_based,
+                          'ichunk_zero_based', outputs.ichunk_zero_based)
+                    # we want the fpga-based one -- ichunk_fpga_based
+                    fpga_chunk_start = outputs.ichunk_fpga_based * fpga_per_chunk
+                    fpga_chunk_end = (outputs.ichunk_fpga_based + 1) * fpga_per_chunk
+                    print('chunk FPGA seq:', fpga_chunk_start, 'to', fpga_chunk_end)
+                    # unix_time_nano_start = fpga0_nano + fpga_chunk_start * nano_per_fpga
+                    # unix_time_nano_end   = fpga0_nano + fpga_chunk_end   * nano_per_fpga
+                    # print('unix nano:', unix_time_nano_start, unix_time_nano_end)
+                    # nano = 1_000_000_000
+                    # date_start = datetime.fromtimestamp(unix_time_nano_start // nano +
+                    #                                     1e-9 * (unix_time_nano_start % nano))
+                    # date_end = datetime.fromtimestamp(unix_time_nano_end // nano +
+                    #                                     1e-9 * (unix_time_nano_end % nano))
+                    # print('chunk:', date_start, 'to', date_end)
+
                 # out_max is a list of ntrees ksgpu.Array objects.
                 for tree_out in outputs.out_max:        # loop over trees
                     #print('Tree_out:', type(tree_out), tree_out.shape)
@@ -97,7 +137,19 @@ def _run_chord_grouper(grouper_addr, sifter_addr, grouper, delay=0.0):
         print(f'{grouper_addr}: ichunk={ichunk}: '
               f'per-beam max =', '[ ' + ', '.join(['%.1f' % b for b in bmax]) + ' ]',
               flush=True)
-        
+
+        # Send to Sifter
+        # (we could coarse-grain the SNR values over more than one chunk...)
+        msg = FrbEventsMessage(has_injections=0,
+                               beam_set_id=beamset,
+                               chunk_fpga_count=fpga_chunk_start,
+                               events=[],
+                               coarsegrain_start_fpga_count=fpga_chunk_start,
+                               coarsegrain_end_fpga_count=fpga_chunk_end,
+                               coarsegrain_snr=bmax)
+        reply = sifter.FrbEvents(msg)
+        print('Got Sifter result:', reply.ok, 'message', reply.message)
+
         if delay > 0:
             time.sleep(delay)
 
